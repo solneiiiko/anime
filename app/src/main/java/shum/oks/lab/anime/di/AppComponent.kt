9d28758d2a040775505e377db372110466257546
solneiiiko <solneiiiko@gmail.com>
2026-06-30 11:49:11 +1000

    ComponentHolderInitializer

---
+++ app/src/main/java/shum/oks/lab/anime/di/AppComponent.kt
@@ -0,0 +1,17 @@
+/*
+ * Copyright © 2026 Oksana Shumilova.
+ * All rights reserved.
+ *
+ * This source code is provided for portfolio and evaluation purposes only.
+ * Unauthorized copying, modification, or distribution is prohibited.
+ */
+
+package shum.oks.lab.anime.di
+
+import dagger.Component
+
+@Component
+interface AppComponent {
+
+    val componentHolderInitializer: ComponentHolderInitializer
+}
