83e1195d3be79ab916de786c4f0675b71ba2be58
solneiiiko <solneiiiko@gmail.com>
2026-07-12 23:36:15 +1000

    Jetpack Paging 3: with bugs

--- feature/catalog/src/main/java/shum/oks/lab/feature/catalog/screens/CatalogScreen.kt
+++ feature/catalog/src/main/java/shum/oks/lab/feature/catalog/screens/CatalogScreen.kt
@@ -8,13 +8,41 @@
 
 package shum.oks.lab.feature.catalog.screens
 
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.paging.LoadState
+import androidx.paging.compose.collectAsLazyPagingItems
+import shum.oks.lab.feature.catalog.di.CatalogUiComponentHolder
+import shum.oks.lab.feature.catalog.screens.composables.AnimeListContent
+import shum.oks.lab.feature.catalog.screens.composables.CatalogSkeleton
+import shum.oks.lab.feature.catalog.screens.composables.FullScreenError
+import shum.oks.lab.feature.catalog.screens.mvi.CatalogUiViewModel
 
 @Composable
 internal fun CatalogScreen(
+    viewModel: CatalogUiViewModel = viewModel(
+        factory = CatalogUiComponentHolder.get().viewModelFactory
+    ),
     modifier: Modifier = Modifier,
 ) {
-    Text("I'm CATALOG")
+    val animePagingData = viewModel.animePagingData.collectAsLazyPagingItems()
+
+    when (val refreshState = animePagingData.loadState.refresh) {
+        is LoadState.Loading -> {
+            CatalogSkeleton(modifier = modifier)
+        }
+        is LoadState.Error -> {
+            FullScreenError(
+                text = refreshState.error.toString(),
+                onRetry = animePagingData::retry,
+            )
+        }
+        is LoadState.NotLoading -> {
+            AnimeListContent(
+                modifier = modifier,
+                pagingItems = animePagingData
+            )
+        }
+    }
 }
