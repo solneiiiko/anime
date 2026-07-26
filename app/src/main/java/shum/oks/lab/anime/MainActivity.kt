d55bf2490ef33b5aec506cbacc0c1be5d060b19a
solneiiiko <solneiiiko@gmail.com>
2026-07-26 14:01:03 +1000

    correct ViewModels' lifecycle

--- app/src/main/java/shum/oks/lab/anime/MainActivity.kt
+++ app/src/main/java/shum/oks/lab/anime/MainActivity.kt
@@ -12,12 +12,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import androidx.lifecycle.viewmodel.compose.viewModel
 import shum.oks.lab.anime.di.AppComponentHolder
 import shum.oks.lab.anime.mvi.AppUiState
 import shum.oks.lab.anime.mvi.AppViewModel
@@ -26,13 +26,16 @@ import shum.oks.lab.common.theme.AnimeTheme
 
 class MainActivity : ComponentActivity() {
 
+    private val appViewModel: AppViewModel by viewModels {
+        AppComponentHolder.get().appViewModelFactory
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
 
         enableEdgeToEdge()
         setContent {
-            val appViewModel: AppViewModel = viewModel(factory = AppComponentHolder.get().appViewModelFactory)
             val state by appViewModel.state.collectAsStateWithLifecycle()
             splashScreen.setKeepOnScreenCondition { state is AppUiState.Loading }
             when (val state = state) {
