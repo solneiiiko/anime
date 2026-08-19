028c3ea78053fc3ca297b3bbe543f9d75382580c
solneiiiko <solneiiiko@gmail.com>
2026-08-19 17:09:53 +1000

    review

--- core/di/build.gradle.kts
+++ core/di/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     id("java-library")
     alias(libs.plugins.anime.kotlin.library)
-    alias(libs.plugins.ksp)
 }
 java {
     sourceCompatibility = JavaVersion.VERSION_11
--- core/network/build.gradle.kts
+++ core/network/build.gradle.kts
@@ -1,5 +1,3 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
     id("java-library")
     alias(libs.plugins.anime.kotlin.library)
